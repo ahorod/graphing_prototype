fd143848b593839e650e1d87ec130f3e990eb8a9
ahorod <prozerpina112009@gmail.com>
2017-08-16 23:22:55 -0700

    add enequality support

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,54 @@
 function formula_input(){
   var fn = Guppy.instances.guppy_div.backend.get_content('text').trim();
+
+  fn = fn.replace('*  <', '<')
+  fn = fn.replace('<  *', '<')
+  fn = fn.replace('*  >', '>')
+  fn = fn.replace('>  *', '>')
+
   var fnType = 'linear';
+  var isInequality = false;
   var is_implicit = fn.split('=').length == 2;
+  var is_inequality =
+    fn.split('<').length == 2
+    || fn.split('>').length == 2;
 
   fn = fn.replace(/\$pi/g, 'PI')
+  console.log(fn);
   if (is_implicit) {
     fnType = 'implicit';
-    fn = '(' + fn.split('=')[0] + ')-(' + fn.split('=')[1] + ')'
+    fn = '(' + fn.split('=')[0] + ')-(' + fn.split('=')[1] + ')';
+    isInequality = false;
+  }
+  if (is_inequality) {
+    fnType = 'implicit';
+    isInequality = true;
+    fn = '(' + fn.split('<')[0] + ')-(' + fn.split('<')[1] + ')';
+    if (fn.split('>').length == 2) {
+      fn = '-(' + fn + ')';
+    }
   }
   console.log(fn);
 
   functionPlot({
     target: '#quadratic-with-options',
-    width: 580,
-    height: 400,
+    width: 870,
+    height: 600,
     disableZoom: true,
     xAxis: {
-      label: 'x - axis',
+      label: 'x',
       domain: [-6, 6]
     },
     yAxis: {
-      label: 'y - axis'
+      label: 'y',
+      domain: [-4, 4]
     },
+    grid: true,
     data: [{
       fn: fn,
-      fnType: fnType
+      fnType: fnType,
+      inequality: isInequality,
+      color: 'rgb(93, 25, 108)'
     }]
   })
 }
